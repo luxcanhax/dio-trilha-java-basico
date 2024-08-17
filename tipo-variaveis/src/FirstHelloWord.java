754b1fadbbf5c45cba5d84c06d9b39cb3311e44f
Lucas Dantas <lucasdan0809@gmail.com>
2024-08-17 14:03:24 -0300

    criando o projeto para exemplo de tipos e variaveis

---
+++ tipo-variaveis/src/FirstHelloWord.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class FirstHelloWord {
+    public static void main(String[] args) {
+    
+        Scanner Reps = new Scanner(System.in);
+
+System.out.println("Hello, Word!");
+
+            Reps.nextLine();
+
+System.out.println("Whait WHAT???? How are you talking to me?? You not supposed to?!?!?");
+
+            Reps.nextLine();
+
+System.out.println("Wtf is happening LOL?");
+
+            Reps.nextLine();
+
+System.out.println("WHAIT N...");
+
+    Reps.close();
+
+    }
+}
