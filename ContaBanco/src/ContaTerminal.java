bbff699fe88b5ebcb7a0f6794ae955ae75699674
Lucas Dantas <lucasdan0809@gmail.com>
2024-08-30 14:26:16 -0300

    so deixei mais bonito o codg

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,8 @@
 
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class ContaTerminal {
+public class ContaTerminal { 
     public static void main(String[] args) {
     
 Scanner num = new Scanner(System.in);
@@ -13,17 +13,19 @@ Scanner nome = new Scanner(System.in);
 
 Scanner valorSaldo = new Scanner(System.in);
 
-System.out.println("Por favor, digite o número da Agência !");
 
-    String agencia = agnc.next();
+
+System.out.println("Por favor, digite o nome do cliente !");    
+
+    String nomeCliente = nome.next();
 
 System.out.println("Por favor, digite o número da Conta !");
   
     int numero = num.nextInt();
 
-System.out.println("Por favor, digite o nome do cliente !");    
+System.out.println("Por favor, digite o número da Agência !");
 
-    String nomeCliente = nome.next();
+    String agencia = agnc.next();
 
 System.out.println(" Por favor, digite o Saldo da conta !");
 
@@ -31,7 +33,7 @@ System.out.println(" Por favor, digite o Saldo da conta !");
 
 System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
-}
+    }
 
 }
   
